9997c1b4e31d74db34e54851dd8a20e4a4cf935f
luftmysza@fedora <dzx01@proton.me>
2025-12-20 02:26:14 +0100

    boilerplate/custom MVC, refactoring

--- Aplikacja/Data/ApplicationDbContext.cs
+++ Aplikacja/Data/ApplicationDbContext.cs
@@ -1,60 +1,112 @@
-﻿using Aplikacja.Models;
+﻿// Initial version ADR 07.12.2025
+// Upd DMI 19.12.2025
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using PROGRAMuj.Models;
 
+namespace PROGRAMuj.Data;
 
-
-namespace Aplikacja.Data
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        User usr1 = new User
         {
-        }
-
-        public DbSet<Kurs> Kursy { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            Id = 1,
+            Name = "Jane Roe"
+        };
+        User usr2 = new User
         {
-            base.OnModelCreating(modelBuilder);
-
-
-            modelBuilder.Entity<Kurs>().HasData(
-                new Kurs
+            Id = 2,
+            Name = "Alex Smith"
+        };
+        User usr3 = new User
+        {
+            Id = 3,
+            Name = "Chris Miller"
+        };
+        User usr4 = new User
+        {
+            Id = 4,
+            Name = "Pat Taylor"
+        };
+        
+        modelBuilder.Entity<User>().HasData(
+            usr1,
+            usr2,
+            usr3,
+            usr4
+        );
+       
+        modelBuilder.Entity<Course>()
+            .HasMany(c => c.Participants)
+            .WithMany();
+        
+        modelBuilder.Entity<Course>()
+            .HasData(
+                new Course
                 {
                     Id = 1,
-                    Tytul = "Wstęp do C#",
-                    Opis = "Poznaj podstawy języka C# i platformy .NET",
-                    JezykProgramowania = "C#",
-                    Cena = 0m
+                    Title = "Wstęp do C#",
+                    Desc = "Poznaj podstawy języka C# i platformy .NET",
+                    ProgLang = "C#",
+                    Price = 0m,
+                    AuthorId = usr1.Id
                 },
-                new Kurs
+                new Course
                 {
                     Id = 2,
-                    Tytul = "Java dla opornych",
-                    Opis = "Obiektowość w praktyce i wzorce projektowe",
-                    JezykProgramowania = "Java",
-                    Cena = 49.99m
+                    Title = "Java dla opornych",
+                    Desc = "Obiektowość w praktyce i wzorce projektowe",
+                    ProgLang = "Java",
+                    Price = 49.99m,
+                    AuthorId = usr2.Id
                 },
-                new Kurs
+                new Course
                 {
                     Id = 3,
-                    Tytul = "HTML & CSS - Budowa stron",
-                    Opis = "Jak stworzyć swoją pierwszą stronę w 15 minut",
-                    JezykProgramowania = "HTML/CSS",
-                    Cena = 0m
+                    Title = "HTML & CSS - Budowa stron",
+                    Desc = "Jak stworzyć swoją pierwszą stronę w 15 minut",
+                    ProgLang = "HTML/CSS",
+                    Price = 0m,
+                    AuthorId = usr3.Id
                 },
-                new Kurs
+                new Course
                 {
                     Id = 4,
-                    Tytul = "Python Data Science",
-                    Opis = "Analiza danych z biblioteką Pandas",
-                    JezykProgramowania = "Python",
-                    Cena = 99.00m
+                    Title = "Python Data Science",
+                    Desc = "Analiza danych z biblioteką Pandas",
+                    ProgLang = "Python",
+                    Price = 99.00m,
+                    AuthorId = usr4.Id
                 }
-            );
-        }
+        );
+
+        modelBuilder.Entity("CourseUser")
+            .HasData(
+                new { CourseId = 1, ParticipantsId = 1 },
+                new { CourseId = 1, ParticipantsId = 2 },
+                new { CourseId = 1, ParticipantsId = 3 },
+                new { CourseId = 1, ParticipantsId = 4 },
+                new { CourseId = 2, ParticipantsId = 1 },
+                new { CourseId = 2, ParticipantsId = 2 },
+                new { CourseId = 2, ParticipantsId = 3 },
+                new { CourseId = 2, ParticipantsId = 4 },
+                new { CourseId = 3, ParticipantsId = 1 },
+                new { CourseId = 3, ParticipantsId = 2 },
+                new { CourseId = 3, ParticipantsId = 3 },
+                new { CourseId = 3, ParticipantsId = 4 },
+                new { CourseId = 4, ParticipantsId = 1 },
+                new { CourseId = 4, ParticipantsId = 2 },
+                new { CourseId = 4, ParticipantsId = 3 },
+                new { CourseId = 4, ParticipantsId = 4 }
+        );
     }
 }
--- Aplikacja/Models/ErrorViewModel.cs
+++ Aplikacja/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-namespace Aplikacja.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace PROGRAMuj.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
+
